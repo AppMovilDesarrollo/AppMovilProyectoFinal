81ad0eb7e23c874a6613933355764fdf6759d209
joesol12 <andres.solano.alvarez@gmail.com>
2017-07-30 15:34:53 -0600

    Pantallas Agregadas
    Se agrega la Pantalla Principal, la pantalla de configuración y la pantalla de detalles del cliente, únicamente la interfaz.  Desarrollador: Andres Solano

---
+++ ProyectoFinal/app/src/main/java/cr/afinal/proyecto/proyectofinal/DetallesCliente.java
@@ -0,0 +1,13 @@
+package cr.afinal.proyecto.proyectofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetallesCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_cliente);
+    }
+}
